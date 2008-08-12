c129e20dc87d3be28bd3dce0b127ea2db71d356b
Evan Broder <broder@mit.edu>
2008-08-12 00:34:32 -0700

    Add a new example showing how to use DictFS

---
+++ routefs/examples/dictexfs.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import routefs
+
+class DictExFS(routefs.DictFS):
+    files = dict(Hello='World',
+                 Directory=dict(a='a', b='b', c=routefs.Symlink('a')))
+
+if __name__ == '__main__':
+    routefs.main(DictExFS)
