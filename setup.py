95faa22cbd3f1fc6dc059fd585cc813de4b886b4
Evan Broder <broder@mit.edu>
2008-09-19 21:14:30 -0400

    Release version 1.0 and set appropriate metadata

--- setup.py
+++ setup.py
@@ -4,12 +4,25 @@ from setuptools import setup, find_packages
 
 setup(
     name="RouteFS",
-    version="0.0.1",
+    version="1.0.0",
     description="RouteFS: A FUSE API wrapper based on URL routing",
     author="Evan Broder",
     author_email="broder@mit.edu",
-    #url="http://ebroder.net/code/RouteFS",
-    license="MIT",
+    url="http://ebroder.net/code/RouteFS",
+    license="MPL, GPL",
     packages=find_packages(),
-    install_requires=['fuse_python>=0.2a', 'Routes>=1.7']
+    install_requires=['fuse_python>=0.2a', 'Routes>=1.7'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Plugins',
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'License :: OSI Approved :: Mozilla Public License 1.1 (MPL 1.1)',
+        'License :: DFSG approved',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python'
+        ]
 )
