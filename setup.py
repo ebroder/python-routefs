f3b3fcb4642075008e5f100ec9f86811ddeabcab
Evan Broder <broder@mit.edu>
2008-12-18 21:59:04 -0600

    Change the RouteFS homepage to Github

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="RouteFS: A FUSE API wrapper based on URL routing",
     author="Evan Broder",
     author_email="broder@mit.edu",
-    url="http://ebroder.net/code/RouteFS",
+    url="http://github.com/ebroder/python-routefs/wikis",
     license="MPL, GPL",
     packages=find_packages(),
     install_requires=['fuse_python>=0.2a', 'Routes>=1.7'],
